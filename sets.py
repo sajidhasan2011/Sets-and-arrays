72d599674ac30bf50e5edb5ee0a7d7bf2adf0efe
Sajid <afrojaakter874@gmail.com>
2024-09-22 19:37:23 +0600

    first commit

---
+++ sets.py
@@ -0,0 +1,16 @@
+print("\033c")
+mySet = {1,2,3}
+print(mySet)
+
+mySet = {1,2,3,4,3,2}
+print(mySet)
+
+li = [1,2,3,2,3,4,5,6,7]
+mySet = set(li)
+
+
+print(mySet)
+
+mySet.pop()
+mySet.pop()
+print(mySet)
